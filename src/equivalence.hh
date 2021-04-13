3afb2807e031a5098fc4555cdb2b82622773f98d
domyang <domyang@ucla.edu>
2021-04-12 17:01:28 -0700

    Adding ability to copy equivalence object

--- src/equivalence.hh
+++ src/equivalence.hh
@@ -12,28 +12,44 @@ class DisjointSet
 		std::vector<unsigned> parents;
 		// For performing union by size
 		std::vector<unsigned> sizes;
-        loooong multiplier;
+        // A count of how many vertices appear in the current match
+        std::vector<unsigned> num_used;
+        loooong multiplier = 1;
         bool has_multiplier = false;
 
 	public:
-		DisjointSet(): count(0), parents(), sizes() {}
+		DisjointSet(): count(0), parents(), sizes(), num_used() {}
 
-		DisjointSet(unsigned n): count(n), parents(), sizes(n, 1)
+		DisjointSet(unsigned n): count(n), parents(), sizes(n, 1), num_used(n, 0)
 		{
 			parents.reserve(n);
 			for (unsigned i = 0; i < n; i++)
 				parents.push_back(i);
 		}
 
+        DisjointSet(const DisjointSet &other): count(other.count), parents(other.count), sizes(other.count), num_used(other.count)
+        {
+            for (unsigned i = 0; i < count; i++)
+            {
+                parents[i] = other.parents[i];
+                sizes[i] = other.sizes[i];
+                num_used[i] = other.num_used[i];
+            }
+            multiplier = other.multiplier;
+            has_multiplier = other.has_multiplier;
+        }
+
 		void add_elems(unsigned n)
 		{
 			count = n;
 			parents.reserve(n);
 			sizes.reserve(n);
+            num_used.reserve(n);
 			for (unsigned i = 0; i < n; i++)
 			{
 				parents.push_back(i);
 				sizes.push_back(1);
+                num_used.push_back(0);
 			}
 		}
 
@@ -73,6 +89,7 @@ class DisjointSet
 
                 parents[y_root] = x_root;
                 sizes[x_root] = sizes[x_root] + sizes[y_root];
+                num_used[x_root] = num_used[x_root] + num_used[y_root];
             }
         }
         
@@ -94,6 +111,62 @@ class DisjointSet
         unsigned get_size(int x)
         {
             return sizes.at(find(x));
+        }
+
+        unsigned get_num_used(int x)
+        {
+            return num_used.at(find(x));
+        }
+
+        void up_num_used(int x)
+        {
+            num_used[find(x)]++;
+        }
+
+        void down_num_used(int x)
+        {
+            num_used[find(x)]--;
+        }
+
+        void restore_base_equivalence()
+        {
+            for (unsigned i = 0; i < count; i++)
+            {
+                parents[i] = i;
+                sizes[i] = 1;
+            }
+            has_multiplier = false;
+        }
+        
+
+        auto operator= (const DisjointSet &other) -> DisjointSet &
+        {
+            if (&other == this)
+                return *this;
+
+            parents.reserve(other.count);
+            sizes.reserve(other.count);
+            num_used.reserve(other.count);
+            for (unsigned i = 0; i < count; i++)
+            {
+                parents[i] = other.parents[i];
+                sizes[i] = other.sizes[i];
+                num_used[i] = other.num_used[i];
+            }
+            multiplier = other.multiplier;
+            has_multiplier = other.has_multiplier;
+
+            return *this;
+        }
+
+        void copy_equivalence(const DisjointSet &other)
+        {
+            for (unsigned i = 0; i < count; i++)
+            {
+                parents[i] = other.parents[i];
+                sizes[i] = other.parents[i];
+            }
+            has_multiplier = false;
         }
 };
 
